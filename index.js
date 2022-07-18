1e7266f92c9c077b3091d28e5a5938aaeaac2b86
LennyRemache <lenny.j.remache@gmail.com>
2022-07-18 19:33:01 -0400

    Added delete functionality to remove tasks from the to do list

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let taskArr = [];
 let list = document.querySelector(".list");
 const userInput = document.querySelector(".user-input");
 const addBtn = document.querySelector(".add-btn");
+let delBtns = [];
 
 addBtn.addEventListener("click", () => {
     const newTask = getTask(taskArr);
@@ -24,32 +25,30 @@ function getTask() {
 
 function addTask(taskList) {
     let allTasks = "";
-    taskList.forEach((task, i) => {
-        const taskID = `del-btn-${i}`;
+    taskList.forEach(task => {
         allTasks += `
-            <li>${task}</li>
-            <button id="${taskID}">X</button>
+            <div class="item-container">
+                <li>${task}</li>
+                <button class="del-btn">X</button>  
+            </div>
         `;
     });
     list.innerHTML = allTasks;
     // spread operator changes nodeList to array of elements
-    const delBtns = [...document.querySelectorAll("button[id^=del-btn-]")];
-    //console.log(btnEl)
     // select all button elements that have an id that start with del-btn-
-    //let delBtn = document.querySelector("button[id^=del-btn-]");
+    //delBtns = [...document.querySelectorAll("button[id^=del-btn-]")];
+
+    delBtns = [...document.querySelectorAll(".del-btn")];
+
     // console.log([...delBtn][0].getAttribute('id'));
-    //[...delBtn][0].addEventListener("click", () => {
 
-    // })
-    console.log(delBtns);
+    // delete task by removing from taskList and re-rendering rather than removing elements directly using document object methods
     delBtns.forEach(btn => {
-        btn.addEventListener("click", {
-
-        })
-    })
-    
-};
-
-function delTask() {
-    
+        btn.addEventListener("click", () => {
+            const id = delBtns.indexOf(btn);
+            taskList.splice(id, 1);
+            addTask(taskList);
+            console.log(delBtns);
+        });
+    });
 };
